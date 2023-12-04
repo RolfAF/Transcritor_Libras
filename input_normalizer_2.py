543a464c7acaf578f31257edd71db9ca2f2b1783
RolfAF <83870936+RolfAF@users.noreply.github.com>
2023-12-04 16:33:26 -0300

    Update input_normalizer_2.py

--- input_normalizer_2.py
+++ input_normalizer_2.py
@@ -29,56 +29,55 @@ while True:
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 #px, py = int(lm.x*1000), int(lm.y*1000)
-                px, py = lm.x*1000, lm.y*1000
                 if id == 0:
                     lm.z = 0
                     ref_cx = cx
                     ref_cy = cy
-                    ref_px = px
-                    ref_py = py
-                cz = lm.z*1000
+                    #ref_px = px
+                    #ref_py = py
+                cz = lm.z*1500
                 rel_cx = cx-ref_cx
                 rel_cy = cy-ref_cy
-                rel_px = px-ref_px
-                rel_py = py-ref_py
-                array.append([rel_px,rel_py,cz])
-                cv2.circle(img, (int(425-cz), cy), 5, (0, 255, 255), cv2.FILLED)
+                #rel_px = px-ref_px
+                #rel_py = py-ref_py
+                array.append([rel_cx,rel_cy,cz])
+
                 #cv2.circle(img, (int(215+rel_cx), cy), 5, (139, 0, 0), cv2.FILLED)
             mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
         #print(results.multi_hand_landmarks[0].landmark[0].y)
+        a = array[9][0]
+        b = array[9][1]
+        c2 = math.pow(a,2) + math.pow(b,2)
+        c = math.sqrt(c2)
+        #array[i].append(c)
+        print("c:",c)
         try:
-            a = array[9][0]
-            b = array[9][1]
-            c2 = pow(a,2)+pow(b,2)
-            c = math.sqrt(c2)
             #alpha = math.degrees(math.atan(a/b)) # 0 -> -90/90 -> 0 -> -90/90 -> 0
             alpha = math.degrees(math.acos(b/c)) # 0 -> 90 -> 180/180 -> 90 -> 0
-            #alpha = math.acos(b/c) # 0 -> 90 -> 180/180 -> 90 -> 0
             if(a > 0):
                 angle = 360 - alpha
-                #angle = 3.28 - alpha
             else:
                 angle = alpha
             ref_angle = 180 - angle
-            #ref_angle = 3.14 - angle
         except Exception as e:
-                print("Erro: ",e)
-        #ref_angle = 90 #int(ref_angle)
-        #ref_angle = 0
+            print("Erro: ",e)
+        print("angle:",angle)
+        aux_x = 200
+        aux_y = h - 10
         ref_angle = math.radians(ref_angle)
-        #print(ref_angle)
-        for i in range(0,21): # <- ponto zero nao incluso...
-            x = array[i][0]
-            y = array[i][1]
-            x2 = 200 + int((x * math.cos(ref_angle)) - (y * math.sin(ref_angle)))
-            y2 = h + int((x * math.sin(ref_angle)) + (y * math.cos(ref_angle)))
-            cv2.circle(img, (int(x2), int(y2)), 5, (0, 0, 255), cv2.FILLED)
-                
-        # calcular c e alpha para todos os pontos, subtrair alpha[9] dos outros pontos, c se torna b
-        # para calcular novo x: a = c * sin(alpha)
-        #https://www.omnicalculator.com/math/angle-of-right-triangle
-        #https://www.omnicalculator.com/math/right-triangle-side-angle
-        
+        ref_scale = c / 200
+        cv2.circle(img, (200, aux_y), 5, (139, 0, 0), cv2.FILLED)
+        cv2.circle(img, (350, aux_y), 5, (139, 0, 0), cv2.FILLED)
+        for i in range(1,21):
+            x = array[i][0] / ref_scale
+            y = array[i][1] / ref_scale
+            x2 = aux_x + (x*math.cos(ref_angle)) - (y*math.sin(ref_angle))
+            y2 = aux_y + (x*math.sin(ref_angle)) + (y*math.cos(ref_angle))
+            array[i].append(x2)
+            array[i].append(y2)
+            print(array[i])
+            cv2.circle(img, (int(array[i][3]), int(array[i][4])), 5, (139, 0, 0), cv2.FILLED)
+            cv2.circle(img, (int(350-array[i][2]), int(aux_y + y)), 5, (139, 0, 0), cv2.FILLED)
         array = []
     # Time and FPS Calculation
     #print(results.multi_hand_landmarks.landmark[0][0])
