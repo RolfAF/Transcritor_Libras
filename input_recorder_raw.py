4daa48e81149ab8744e0b2d61c446b0a24508ca1
RolfAF <83870936+RolfAF@users.noreply.github.com>
2023-12-04 16:33:54 -0300

    Update input_recorder_raw.py

--- input_recorder_raw.py
+++ input_recorder_raw.py
@@ -10,38 +10,59 @@ line = ''
 cont = 0
 cTime = 0
 pTime = 0# Function Start
+array = []
+gesture = 20
+filename = "raw_y"
 time.sleep(1)
 inp = input("Iniciar leitura\n")
-while(cont <= 100):
-    line = str(cont)+":"
+while(cont < 1100):
+    #line = str(cont)+":"
     success, img = cap.read()
     img = cv2.flip(img, 1)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     #print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
-        file_a = open("a.txt","a")
+        file_ = open(filename,"a")
         for handlms in results.multi_hand_landmarks:
             for id, lm in enumerate(handlms.landmark):
                 h, w, c = img.shape
-                cx, cy = int(lm.x*w), int(lm.y*h)
+                cx, cy = lm.x*w, lm.y*h
+                px, py = lm.x*1000, lm.y*1000
                 if id == 0:
                     lm.z = 0
                     ref_x = cx
                     ref_y = cy
+                    ref_px = px
+                    ref_py = py
                 #print(id, lm)
                 #cx, cy = lm.x, lm.y
                 cz = lm.z*1000
                 rel_x = int(cx-ref_x)
                 rel_y = int(cy-ref_y)
-                print(id, lm.x, lm.y, lm.z)
-                line += " "+str(lm.x)+" "+str(lm.y)+" "+str(lm.z)
-                #if id == 5:
-                cv2.circle(img, (int(425-cz), cy), 5, (139, 0, 0), cv2.FILLED)
-                cv2.circle(img, (int(215+rel_x), cy), 5, (139, 0, 0), cv2.FILLED)
+                rel_px = px-ref_px
+                rel_py = py-ref_py
+                #print(id, lm.x, lm.y, lm.z)
+                if id > 0:
+                    line += str(rel_px)+","+str(rel_py)+","+str(cz)+","
+                array.append(cx)
+                array.append(cy)
+                array.append(cz)
+                cv2.circle(img, (int(425-cz), int(cy)), 5, (139, 0, 0), cv2.FILLED)
+                cv2.circle(img, (int(215+rel_x), int(cy)), 5, (139, 0, 0), cv2.FILLED)
             mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
-        file_a.write(line+"\n")
-        file_a.close()
+        if(len(array) == 63):
+            array.append(gesture)
+            line += str(gesture)
+            #hand_np_array = np.array(array)
+            #print(line)
+            if(cont > 99):
+                file_.write(line+"\n")
+                file_.close()
+            cont += 1
+            print(cont-99,array)
+        array = []
+        line = ''
     # Time and FPS Calculation
     cTime = time.time()
     fps = 1/(cTime-pTime)
@@ -51,4 +72,3 @@ while(cont <= 100):
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-    cont += 1
