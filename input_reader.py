acd98295fc064440ad0e34a7c0210ccbd8f05e69
RolfAF <83870936+RolfAF@users.noreply.github.com>
2023-12-05 18:51:32 -0300

    Update input_reader.py

--- input_reader.py
+++ input_reader.py
@@ -4,10 +4,10 @@ import time
 import numpy as np
 import math
 
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 #mpHands = mp.solutions.hands
 #hands = mpHands.Hands(max_num_hands=1,model_complexity=1,min_detection_confidence=0.9,min_tracking_confidence=0.9)
-mpDraw = mp.solutions.drawing_utils
+#mpDraw = mp.solutions.drawing_utils
 cTime = 0
 pTime = 0# Function Start
 array = []
@@ -18,34 +18,29 @@ time.sleep(1)
 aux_lines = []
 point = []
 points = []
-
+h = 480
+w = 640
 #inp = input("Iniciar leitura\n")
 f = open("raw/raw_a.txt", "r")
 lines = f.readlines()
 #print(lines)
 #print(lines[0])
-for i in range(0,59):
-    aux_lines.append(lines[0].split(","))
-for i in range(0,1):
-    point.append(aux_lines[i*2])
-    point.append(aux_lines[(i*2)+1])
-    point.append(aux_lines[(i*2)+2])
-    print(aux_lines[i*2])
-    print(aux_lines[(i*2)+1])
-    print(aux_lines[(i*2)+2])
-    #points.append(point)
-    #print(point[i])
-    #point = []
-#print(aux_lines[0][60])
-#print(points[0][0][0])
+for i in range(0,len(lines)): #separa todos os elementos de cada linha
+    img = np.zeros((h,w,3), np.uint8)
+    lines[i] = lines[i].split(",")
+    for j in range(0,60,3): #agrupa as coordenadas x, y e z de cada ponto, e agrupa os 20 pontos
+        point.append(float(lines[i][j]))
+        point.append(float(lines[i][j+1]))
+        point.append(float(lines[i][j+2]))
+        points.append(point)
+        point = []
+    lines[i] = points #guarda os pontos na linha
+    points = []
 
-"""
-for i in range(0,1):
-    a = array[i][0]
-    b = array[9][1]
+    a = lines[i][10][0]
+    b = lines[i][10][1]
     c2 = math.pow(a,2) + math.pow(b,2)
     c = math.sqrt(c2)
-    #array[i].append(c)
     print("c:",c)
     try:
         #alpha = math.degrees(math.atan(a/b)) # 0 -> -90/90 -> 0 -> -90/90 -> 0
@@ -64,16 +59,19 @@ for i in range(0,1):
     ref_scale = c / 200
     cv2.circle(img, (200, aux_y), 5, (139, 0, 0), cv2.FILLED)
     cv2.circle(img, (350, aux_y), 5, (139, 0, 0), cv2.FILLED)
-    for i in range(1,21):
-        x = array[i][0] / ref_scale
-        y = array[i][1] / ref_scale
+    for j in range(0,20):
+        x = lines[i][j][0] / ref_scale
+        y = lines[i][j][1] / ref_scale
         x2 = aux_x + (x*math.cos(ref_angle)) - (y*math.sin(ref_angle))
         y2 = aux_y + (x*math.sin(ref_angle)) + (y*math.cos(ref_angle))
-        array[i].append(x2)
-        array[i].append(y2)
-        print(array[i])
-        cv2.circle(img, (int(array[i][3]), int(array[i][4])), 5, (139, 0, 0), cv2.FILLED)
-        cv2.circle(img, (int(350-array[i][2]), int(aux_y + y)), 5, (139, 0, 0), cv2.FILLED)
-    array = []
+        lines[i][j][0] = int(x2)
+        lines[i][j][1] = int(y2)
+        #array[j].append(x2)
+        #array[j].append(y2)
+        print(lines[i][j])
+        cv2.circle(img, (int(lines[i][j][0]), int(lines[i][j][1])), 5, (139, 0, 0), cv2.FILLED)
+        cv2.circle(img, (int(350-float(lines[i][j][2])), int(aux_y + y)), 5, (139, 0, 0), cv2.FILLED)
+    #array = []
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
 #cv2.circle(img, (int(array[i][3]), int(array[i][4])), 5, (139, 0, 0), cv2.FILLED)
-"""
