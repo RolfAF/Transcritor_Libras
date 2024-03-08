01b75834ac3f63302254b47de6b1d6b978364d65
RolfAF <83870936+RolfAF@users.noreply.github.com>
2024-03-08 15:34:49 -0300

    Update model_tester_multi.py

--- model_tester_multi.py
+++ model_tester_multi.py
@@ -1,4 +1,4 @@
-from multiprocessing import Pool
+from multiprocessing import Process, Manager, Value
 import cv2
 import mediapipe as mp
 import time
@@ -11,6 +11,8 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=1,model_complexity=1,min_detection_confidence=0.9,min_tracking_confidence=0.9)
 mpDraw = mp.solutions.drawing_utils
+#manager = multiprocessing.Manager()
+#manager.start()
 cTime = 0
 pTime = 0# Function Start
 array = []
@@ -19,26 +21,49 @@ sample.append([])
 gesture = 0
 line = ''
 cont = 0
+last_p = 1
 time.sleep(1)
 prefix = ["a","b","c","d","e","f","g","i","l","m","n","o","p","q","r","s","t","u","v","w","x","y"]
-model0 = load_model('model_3')
+model = load_model('model_3')
+#sample_array = Manager().list([Manager().list()])
+sample_array = Manager().list()
 #model1 = load_model('model_3')
 #model2 = load_model('model_3')
 #model3 = load_model('model_3')
-def prediction(smp):
-    from tensorflow.keras.models import load_model
+def prediction(sample_array):
+    print("asd")
+    #from tensorflow.keras.models import load_model
     model = load_model('model_3')
-    print(model)
+    sample = []
+    while True:
+        #print("process: ",len(sample_array))
+        if len(sample_array) > 0:
+            sample = sample_array[0]
+            sample_array.pop(0)
+            #print(sample)
+            predictions = model.predict(pd.DataFrame(sample))
+            index = str(np.argmax(predictions, axis=1))
+            index = index.replace('[','')
+            index = index.replace(']','')
+            print("gesto:",prefix[int(index)])
+    #return index
+def prediction0(smp):
+    #from tensorflow.keras.models import load_model
+    #import pandas as pd
+    #model = load_model('model_3')
+    #print(mdl)
+    #print(smp)
     predictions = model.predict(pd.DataFrame(smp))
-    print(predictions)
+    #print(predictions)
     #print(np.argmax(predictions, axis=1))
     #print(prefix[int(np.argmax(predictions, axis=1))])
     index = str(np.argmax(predictions, axis=1))
     index = index.replace('[','')
     index = index.replace(']','')
-    #print("gesto:",prefix[int(index)])
-    return index
-
+    print("gesto:",prefix[int(index)])
+    #return index
+#p0 = Process(target=prediction, args=())
+#p0.start()
 while True:
     line = ''
     success, img = cap.read()
@@ -95,13 +120,35 @@ while True:
             sample[0].append(array[i][0])
             sample[0].append(array[i][1])
             sample[0].append(array[i][2])
+        sample_array.append(sample)
+        #print(sample)
+        #sample_array[0] = sample
+        print("main: ",len(sample_array))
         if __name__ == '__main__':
-            p = Pool(processes = 4)
-            res = p.apply_async(prediction, (sample,))
-            print(res.get(timeout = 10))
-            #p0 = Process(target=prediction, args=(model0,sample,))
+            with Manager() as manager:
+                processes = [Process(target=prediction, args=(sample_array,)) for i in range(1)]
+                # start all processes
+                for process in processes:
+                    process.start()
+                #p1 = Process(target=prediction, args=(sample_array[]))
+            #    p1.start()
+            #p = Pool(processes = 4)
+            #res = p.apply_async(prediction, (sample,))
+            #print(res.get(timeout = 10))
+            #print(p0.is_alive)
+            #if last_p == 0:
+            #    p1 = Process(target=prediction, args=(model,sample,))
+            #    p1.start()
+            #    last_p = 1
+            #    print("starting 1")
+            #else:
+            #    p0 = Process(target=prediction, args=(model,sample,))
+            #    p0.start()
+            #    last_p = 0
+            #    print("starting 0")
+            #p0 = Process(target=prediction, args=(sample,))
             #p0.start()
-            #prediction(model0,sample)
+            #prediction(sample)
             #time.sleep(10)
             #p0.terminate()
             #p.join()
